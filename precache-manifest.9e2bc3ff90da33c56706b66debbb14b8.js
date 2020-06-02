98fcc7be67a47647b7b22383f79c662cf033e12d
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-06-02 14:43:09 +0200

    Updates

---
+++ precache-manifest.9e2bc3ff90da33c56706b66debbb14b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa27742d6ea3b99eb80bd4f47d55a63",
+    "url": "/7-Evernote-clone/index.html"
+  },
+  {
+    "revision": "4085c7225c0a5bec6cbe",
+    "url": "/7-Evernote-clone/static/css/main.9e8d25d4.chunk.css"
+  },
+  {
+    "revision": "43db25bb4cda7e2f49bf",
+    "url": "/7-Evernote-clone/static/js/2.51b016bf.chunk.js"
+  },
+  {
+    "revision": "27861f745f0cd649e523632aa61ef20a",
+    "url": "/7-Evernote-clone/static/js/2.51b016bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4085c7225c0a5bec6cbe",
+    "url": "/7-Evernote-clone/static/js/main.18cb56fd.chunk.js"
+  },
+  {
+    "revision": "97c911e7d31300ceeab2",
+    "url": "/7-Evernote-clone/static/js/runtime-main.52e126a9.js"
+  }
+]);
